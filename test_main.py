a1b2d40a943f554b0c2a2afca2e8e983a86cfb33
elinard <elinard95@gmail.com>
2025-07-17 20:17:13 +0500

    добавление тестов на позитивный и негативный поиск. Добавление фикстуры на установку размера браузера

--- test_main.py
+++ test_main.py
@@ -4,11 +4,11 @@ from selene import browser, be, have
 def test_search_with_results(browser_settings):
     browser.open('https://duckduckgo.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text("Selene: User-Oriented Web UI Browser Tests in Python "))
+    browser.element('html').should(have.text("Selene: User-Oriented Web UI Browser Tests in Python "))
 
 
 def test_search_without_results(browser_settings):
     browser.open('https://duckduckgo.com')
     browser.element('[name="q"]').should(be.blank).type('fgdfgdfgdfdвквп').press_enter()
-    browser.element('.card-section').should(have.text("По запросу «fgdfgdfgdfdвквп» ничего не найдено."))
+    browser.element('html').should(have.text("По запросу «fgdfgdfgdfdвквп» ничего не найдено."))
 
